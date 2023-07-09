defcac99e31f7ed8c27e0c3b5bf8bb06a8c54e94
palash2397 <palashpatil55@gmail.com>
2023-07-09 20:01:57 +0530

    first commit

--- test/AstNftSale.ts
+++ test/AstNftSale.ts
@@ -113,16 +113,6 @@ describe("Unit Tests", function () {
             // var tx = await astNft.connect(user).buyPresale(1,{value: (1*( 0.1*10**18)).toString()});
             // var txn = await tx.wait();
 
-            // await ethers.provider.send("evm_increaseTime", [365*24*60*60])
-            // await ethers.provider.send("evm_mine")
-            // var tx = await astReward.getRewardsCalc(0, 1, user.address)
-            // console.log("reward", parseInt(tx))
-            // await token.connect(admin).transfer( astReward.address, ("1000000000000000000000".toString()));
-            // await astReward.connect(user).claim();
-            // await ethers.provider.send("evm_increaseTime", [((365*2) + 300)*24*60*60])
-            // await ethers.provider.send("evm_mine")
-            // tx = await astReward.getRewardsCalc(0, 1, user.address)
-            // console.log("reward", parseInt(tx))
             
         })
         
